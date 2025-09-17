afacbe02fb1b14321854a311c180316576409580
miguelcastrontc <silva.miguelcastro7@gmail.com>
2025-09-17 14:50:04 -0300

    Update and rename file1_name.py to calculator.py

---
+++ package_name/calculator.py
@@ -0,0 +1,21 @@
+
+#Sum 2 numbers
+def sum(a,b):
+    return  a + b 
+
+#sub 2 numbers
+def sub(a,b):
+    return a - b
+
+#mult 2 numbers
+def mul(a,b):
+    return a * b
+
+#div 2 number
+def div(a,b):
+    if a == 0 or b == 0:
+        print('There are no divisions by 0')
+
+    else:
+        return a / b
+    
