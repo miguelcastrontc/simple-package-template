d6253eabbf96ec706949d1673b81b618c76143f2
miguelcastrontc <silva.miguelcastro7@gmail.com>
2025-09-17 14:30:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="simple_package_teste",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Miguel_Castro",
+    author_email="silva.miguelcastro7@gmail.com",
+    description="First package",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/miguelcastrontc/simple-package-template"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
